f612d8c0aad36d67a422890d99d73b0acbfb2953
Russell Eshiet <russelleshiet107@gmail.com>
2026-03-24 15:10:57 +0100

    implemented password validation

--- js/register.js
+++ js/register.js
@@ -5,6 +5,27 @@ e.preventDefault();
 const name = document.getElementById("name").value;
 const email = document.getElementById("email").value;
 const password = document.getElementById("password").value;
+const passwordError = document.getElementById("passwordError");
+const confirmPassword = document.getElementById("confirmPassword").value;
+
+if (password.length < 6 ) {
+    passwordError.textContent = "Password must be at least six characters";
+    document.getElementById("password").addEventListener("input", function(){
+    passwordError.textContent = "";
+    });
+    return;
+}
+
+if (password !== confirmPassword ) {
+    passwordError.textContent = "Passwords do not match";
+    document.getElementById("confirmPassword").addEventListener("input", function(){
+    passwordError.textContent = "";
+    });
+    return;
+}
+
+
+
 
 try{
 
