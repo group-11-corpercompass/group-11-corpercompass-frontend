9a279c7aa037c4c0c3d1acc49d7de98db5f24209
Russell952 <russelleshiet107@gmail.com>
2026-03-24 10:43:27 +0100

    Revert "Reign"

--- css/js/env.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  API_BASE: 'http://localhost:5000/api', // Change to your production backend URL
-  MAPS_API_KEY: 'YOUR_GOOGLE_MAPS_API_KEY',
-  GEOCODER: 'nominatim',
-};
-
-export default config;
