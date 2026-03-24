0b052ddb05c6d606be8e713c529da71cad5632a0
Justus Okoro <jusreal2@gmail.com>
2026-03-24 15:03:51 +0100

    Add environment configuration file env.js

---
+++ js/env.js
@@ -0,0 +1,8 @@
+// Environment configuration
+const config = {
+  API_BASE: 'http://localhost:5001/api', // Change to production URL
+  MAPS_API_KEY: 'YOUR_GOOGLE_MAPS_API_KEY',
+  GEOCODER: 'nominatim',
+};
+
+module.exports = config;
